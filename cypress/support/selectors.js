43d58c0dde64c5ed8aa1baa60d822c62ed7240b9
Andrey Katsevman <vasily12@gmail.com>
2022-12-11 22:28:28 -0500

    update

--- cypress/support/selectors.js
+++ cypress/support/selectors.js
@@ -14,12 +14,23 @@ export default {
     City_selector:'[name="stage2"]',
     Stories_selector:'[name="stories"]',
     House_age:'[name="houseage"]',
+    Gas_Therm_label:'#step1col3 > :nth-child(1)',
     Gas_Therm_field:'[name="gastherm"]',
+    Propane_Gal_label:'#step1col3 > :nth-child(2)',
     Propane_Gal_field:'[name="propaneGas"]',
+    Oil_Gal_label:'#step1col3 > :nth-child(3)',
     Oil_Gal_field:'[name="oilgal"]',
+    Summer_KWHR_label:'#step1col3 > :nth-child(4)',
     Summer_KWHR_field:'[name="skwhr"]',
+    Winter_KWHR_label:'#step1col3 > :nth-child(5)',
     Winter_KWHR_field:'[name="wkwhr"]',
     Squar_feet_selector:'[name="squarefeet"]',
+    Current_Heating_System_label:'#step2col2 > :nth-child(1)',
+    Current_Heating_Cost_label:'#step2col3 > :nth-child(3)',
+    New_Heating_System_label:'#step2col2 > :nth-child(3)',
+    New_Heating_cost_label:'#step2col3 > :nth-child(5)',
+
+
 
 
     Provinces:
