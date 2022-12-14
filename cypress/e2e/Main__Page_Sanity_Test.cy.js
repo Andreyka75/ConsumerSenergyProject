38f00f0eecb1b573a2ccbd806ffe28442068b490
Andrey Katsevman <vasily12@gmail.com>
2022-12-14 11:22:18 -0500

    Update

--- cypress/e2e/Main__Page_Sanity_Test.cy.js
+++ cypress/e2e/Main__Page_Sanity_Test.cy.js
@@ -3,59 +3,59 @@
 // import selectors from '../support/selectors.js'
 // import commands from '../support/commands.js'
 
-describe('Main Page Sanity testing', () => {
+describe('Main Page Sanity testing',{defaultCommandTimeout: 60000}, () => {
   beforeEach('navigate to the main page', () => {
     cy.visit('/')
     
   })
-  it('verify correct URL',{defaultCommandTimeout: 60000},()=>{
+  it('verify correct URL',()=>{
     cy.url().should('eq', 'https://consumersenergymanagement.ca/')
   })
-  it('verify clicking on Home tab',{defaultCommandTimeout: 60000}, () => {
+  it('verify clicking on Home tab', () => {
     
     cy.clickOnHomeTab()
   })
-  it('verify clicking on About tab',{defaultCommandTimeout: 60000}, () => {
+  it('verify clicking on About tab', () => {
     
     cy.clickOnAboutTab()
 
   })
-  it('verify clicking on Products tab',{defaultCommandTimeout: 60000}, () => {
+  it('verify clicking on Products tab', () => {
     
     cy.clickOnProductsTab()
 
   })
-  it('verify clicking on Services tab',{defaultCommandTimeout: 60000}, () => {
+  it('verify clicking on Services tab', () => {
     
     cy.clickOnServicesTab()
 
   })
-  it('verify clicking on Tools tab',{defaultCommandTimeout: 60000}, () => {
+  it('verify clicking on Tools tab', () => {
     
     cy.clickOnToolsTab()
 
   })
-  it('verify clicking on Promotions tab',{defaultCommandTimeout: 60000}, () => {
+  it('verify clicking on Promotions tab', () => {
     
     cy.clickOnPromotionsTab()
 
   })
-  it('verify clicking on Careers tab',{defaultCommandTimeout: 60000}, () => {
+  it('verify clicking on Careers tab', () => {
     
     cy.clickOnCareersTab()
 
   })
-  it('verify clicking on Blog tab',{defaultCommandTimeout: 60000}, () => {
+  it('verify clicking on Blog tab', () => {
     
     cy.clickOnBlogTab()
 
   })
-  it('verify clicking on FAQs tab',{defaultCommandTimeout: 60000}, () => {
+  it('verify clicking on FAQs tab', () => {
     
     cy.clickOnFAQsTab()
 
   })
-  it('verify clicking on Contact tab',{defaultCommandTimeout: 60000}, () => {
+  it('verify clicking on Contact tab', () => {
     
     cy.clickOnContactTab()
 
